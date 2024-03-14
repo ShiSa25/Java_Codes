43ca30a3024c8e30f176079c1cad4ffbbaca38f9
Shivanshu Sahu <68166464+ShiSa25@users.noreply.github.com>
2024-03-14 22:53:59 +0530

    created TempConv.java

---
+++ TempConv.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TempConv {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the temp in Celcious:");
+        int temp = in.nextInt();
+        float temp1 = (temp * 9/5) + 32 ;
+        System.out.println("The temp in Fahrenheit is: " + temp1);
+        in.close();
+
+
+    }
+
+
+}
